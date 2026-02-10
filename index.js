b2345eb3f66f24ed7d7e8fa541523164b3e94cbc
hc-hyuk <65274040+hc-hyuk@users.noreply.github.com>
2026-02-10 13:38:08 +0900

    완성버전 2번째 강의에 달성
    완성버전 2번째 강의에 달성

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.json('Hello World')
 })
-app.get('/user/:id', (req, res) => {
-  //const q=req.params
-  //console.log(q.id)
-  const q=req.query
-  console.log(q.q)
-  console.log(q.name)
-  res.json({'userid':q.name})
-})
-app.get('/cat', (req, res) => {
-  res.json({'sound':'멍멍'})
+app.get('/sound/:name', (req, res) => {
+  const{ name }=req.params
+  if(name=="dog"){
+    res.json({'sound':'멍멍'})
+  }
+  else if(name=="cat"){
+    res.json({'sound':'야옹'})
+  }
+  else if(name=="pig"){
+    res.json({'sound':'꿀꿀'})
+  }
+  else {
+    res.json({'sound':'알수없음'})
+  }
 })
 app.listen(port, () => {
-  console.log('Example app listening on port ${port}')
+  console.log(`Example app listening on port ${port}`)// 작은 따옴표가 아닌, 백틱으로 감싸줌
 })
